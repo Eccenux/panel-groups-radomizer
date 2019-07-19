048b3c4882043b194c2bdb2f8e3c62f7fdec44dc
Eccenux <eccenux+git@gmail.com>
2019-07-19 18:37:04 +0200

    settings form

--- app/_locales/en/messages.js
+++ app/_locales/en/messages.js
@@ -16,5 +16,25 @@ export default {
 	},
 	"results.Results_summary": {
 		"message": "Results summary"
-	}
+	},
+	"settings.opening_information": {
+		"message": "Opening information"
+	},
+	"settings.Default_info": {
+		"message": `
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean malesuada tincidunt ligula vitae dictum. Phasellus et tellus tortor. In luctus vel nisi eu scelerisque. Quisque eu congue velit. Aenean elementum elit fermentum bibendum rutrum. Pellentesque rhoncus iaculis lacus ultricies vestibulum. Proin sit amet lacus urna.
+		`.replace(/^\s+/, '').replace(/\n\t+/, '\n')
+	},
+	"settings.total_count": {
+		"message": "Total count"
+	},
+	"settings.group_max": {
+		"message": "Group max"
+	},
+	"settings.stages_count": {
+		"message": "Stages count"
+	},
+	"settings.Generate": {
+		"message": "Generate"
+	},
 }
--- app/_locales/pl/messages.js
+++ app/_locales/pl/messages.js
@@ -12,4 +12,24 @@ export default {
 	"results.Results_summary": {
 		"message": "Podsumowanie losowania"
 	},
+	"settings.opening_information": {
+		"message": "Tekst informacyjny"
+	},
+	"settings.Default_info": {
+		"message": `
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean malesuada tincidunt ligula vitae dictum. Phasellus et tellus tortor. In luctus vel nisi eu scelerisque. Quisque eu congue velit. Aenean elementum elit fermentum bibendum rutrum. Pellentesque rhoncus iaculis lacus ultricies vestibulum. Proin sit amet lacus urna.
+		`.replace(/^\s+/, '').replace(/\n\t+/, '\n')
+	},
+	"settings.total_count": {
+		"message": "Liczba osób"
+	},
+	"settings.group_max": {
+		"message": "Wielkość grup"
+	},
+	"settings.stages_count": {
+		"message": "Liczba dyskusji"
+	},
+	"settings.Generate": {
+		"message": "Generate"
+	},
 }
