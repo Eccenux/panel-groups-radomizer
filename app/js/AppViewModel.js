943135c535a2b92ea2e1cd2aeb345b3b1ad7d982
Eccenux <eccenux+git@gmail.com>
2019-07-22 03:32:07 +0200

    Group generation results model

--- app/js/AppViewModel.js
+++ app/js/AppViewModel.js
@@ -1,19 +1,24 @@
 import {SettingsModel} from './SettingsModel.js';
+import {ResultsModel} from './ResultsModel.js';
 import {GroupRandomizer} from './GroupRandomizer.js';
 
 class AppViewModel {
 	constructor() {
 		this.cards = ko.observableArray([1,2]);
 		this.settings = new SettingsModel();
+		this.results = new ResultsModel();
 		this.stagesCount = 6;
 	}
 
 	generateClick() {
-		this.settings.update();
+		let settings = this.settings.readForm();
+		this.settings.update(settings);
 		let gr = new GroupRandomizer(
-			this.settings.totalCount(), this.settings.groupMax(), this.settings.stagesCount()
+			settings.totalCount, settings.groupMax, settings.stagesCount
 		);
+		app.gr = gr;	// keep ref in the app
 		gr.run();
+		this.results.update(settings, gr);
 	}
 }
 
---
+++ app/js/ResultsModel.js
@@ -0,0 +1,56 @@
+import {SettingsModel} from './SettingsModel.js';
+import {GroupRandomizer} from './GroupRandomizer.js';
+
+/**
+ * Group generation results.
+ */
+class ResultsModel extends SettingsModel {
+	constructor() {
+		super();
+
+		/**
+		 * Group numbers for each person.
+		 * 
+		 * person index -> group numbers per stage
+		 * 
+		 * e.g:
+		 * 0: 3,5,6,7,6,1,5,2,8,5
+		 * 1: 4,1,5,6,6,8,4,1,6,8
+		 * ...
+		 */
+		this.groupNumbers = ko.observableArray();
+
+		// stats
+		this.groupSizes = ko.observableArray();
+		this.minGroupSize = ko.observable(0);
+		this.maxGroupSize = ko.observable(0);
+	}
+
+	/**
+	 * Update properties.
+	 * @param {Object} settings 
+	 * @param {GroupRandomizer} gr
+	 */
+	update(settings, gr) {
+		super.update(settings);
+
+		// copy mapping
+		this.groupNumbers(JSON.parse(JSON.stringify(gr.results.memberGroups)));
+
+		// calculate sizes
+		let groupSizes = gr.groups.map(group=>group.members.length);
+		this.groupSizes(groupSizes);
+		if (groupSizes.length) {
+			this.minGroupSize(groupSizes.reduce((min, current) => Math.min(min, current)));
+			this.maxGroupSize(groupSizes.reduce((max, current) => Math.max(max, current)));
+		} else {
+			this.minGroupSize(0);
+			this.maxGroupSize(0);
+		}
+
+		//console.log('groupSizes: ', groupSizes.join(','));
+		//gr.results.dump();
+	}
+}
+
+export {ResultsModel}
